3d15753d0cb88bf49d78d3222fe2cc88c05e8efb
Hanno Lemoine <Hanno.Lemoine@ruhr-uni-bochum.de>
2012-02-02 15:51:44 -0500

    add are_patch
    deps/python-magic has a git-repo
    this cannot agree together with the are-git-repo
    so we disarm the subrepo in python-magic by
    renaming .git into .git.bak

--- tools/are_patch_bashrc.sh
+++ tools/are_patch_bashrc.sh
@@ -4,10 +4,14 @@
 #author: Hanno Lemoine
 ###########################
 
+echo "FIX Bashrc"
 echo "" >> /home/android/.bashrc
 echo "#ARE Submodul (The Honeynet Project)" >> /home/android/.bashrc
 echo "if [ -f ~/tools/.bash_are ]; then" >> /home/android/.bashrc
 echo "     . ~/tools/.bash_are" >> /home/android/.bashrc
 echo "fi" >> /home/android/.bashrc
+
+echo "Deactivate git-subrepo in deps/python-magic/"
+mv /home/android/deps/python-magic/.git /home/android/deps/python-magic/.git.bak
 
 
